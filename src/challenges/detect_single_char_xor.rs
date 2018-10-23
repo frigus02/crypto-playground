87d0c5ce437c3c133d4769a41238216ef5841ba7
Jan Kuehle <jkuehle90@gmail.com>
2018-10-23 22:34:21 +0100

    Use letter frequency table for single char xor detection

--- src/challenges/detect_single_char_xor.rs
+++ src/challenges/detect_single_char_xor.rs
@@ -1,13 +1,12 @@
 use super::super::encoding::hex;
 use super::super::encoding::xor;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io;
+use std::io::prelude::BufRead;
 
 const ASCII_SPACE: u8 = 32;
-const ASCII_NUM_0: u8 = 48;
-const ASCII_NUM_9: u8 = 57;
-const ASCII_UPPER_A: u8 = 65;
-const ASCII_UPPER_Z: u8 = 90;
-const ASCII_LOWER_A: u8 = 97;
-const ASCII_LOWER_Z: u8 = 122;
 const ASCII_DEL: u8 = 127;
 
 pub struct Info {
@@ -15,58 +14,88 @@ pub struct Info {
     pub plain_text: String,
 }
 
-pub fn run(hex: &str) -> Result<Info, &'static str> {
+pub fn run(hex: &str) -> Result<Info, String> {
+    let en_letter_frequency = get_en_letter_frequency()
+        .map_err(|err| format!("reading letter_frequency_en failed: {}", err))?;
     let bytes = hex::decode(hex);
+    let key_range = ASCII_SPACE..ASCII_DEL;
 
-    for key in ASCII_SPACE..ASCII_DEL {
-        let encoded_bytes = xor::encode(&bytes, &vec![key]);
-        if is_ascii_char(&encoded_bytes[0])
-            && is_in_ascii_range(&encoded_bytes)
-            && get_ascii_char_sign_ratio(&encoded_bytes) < 0.5
-            && get_ascii_char_space_ratio(&encoded_bytes) > 0.05
-        {
-            let plain_text = String::from_utf8(encoded_bytes).unwrap();
-            return Ok(Info { key, plain_text });
+    let key = key_range
+        .into_iter()
+        .map(|key| {
+            let encoded_bytes = xor::encode(&bytes, &vec![key]);
+
+            let byte_frequency = get_byte_frequency(&encoded_bytes);
+            let differences: Vec<f64> = byte_frequency
+                .iter()
+                .map(|(letter, frequency)| {
+                    en_letter_frequency
+                        .get(letter)
+                        .map_or(frequency.clone(), |en_frequency| {
+                            (frequency - en_frequency).abs()
+                        })
+                }).collect();
+
+            let score = differences.iter().sum::<f64>() / differences.len() as f64;
+
+            (score, key)
+        }).min_by(|(score_x, _), (score_y, _)| {
+            score_x.partial_cmp(score_y).unwrap_or(Ordering::Greater)
+        }).ok_or(String::from("score collection empty"))?
+        .1;
+
+    let decoded_bytes = xor::encode(&bytes, &vec![key]);
+    return match String::from_utf8(decoded_bytes) {
+        Ok(plain_text) => Ok(Info { key, plain_text }),
+        Err(err) => Err(format!("decoding failed: {}", err)),
+    };
+}
+
+fn get_en_letter_frequency() -> Result<HashMap<u8, f64>, io::Error> {
+    let file = File::open("res/letter_frequency_en.txt")?;
+    let reader = io::BufReader::new(file);
+    let mut scores = HashMap::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let line = line.trim();
+        if line.len() == 0 {
+            continue;
         }
+
+        let parts: Vec<&str> = line.split(' ').collect();
+        if parts.len() != 2 || parts[0].len() != 1 {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "line does not contain space separated letter and frequency",
+            ));
+        }
+
+        let letter = parts[0].as_bytes()[0];
+        let frequency = parts[1]
+            .parse::<f64>()
+            .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "cannot parse frequency"))?;
+
+        scores.insert(letter, frequency);
     }
 
-    return Err("could not find decoding char");
+    return Ok(scores);
 }
 
-fn is_in_ascii_range(bytes: &[u8]) -> bool {
-    for byte in bytes {
-        if byte >= &ASCII_DEL {
-            return false;
-        }
+fn get_byte_frequency(bytes: &[u8]) -> HashMap<u8, f64> {
+    let mut scores = bytes.iter().fold(HashMap::new(), |mut acc, byte| {
+        acc.entry(byte.clone())
+            .and_modify(|count| *count += 1.0)
+            .or_insert(1.0);
+        acc
+    });
+
+    let len = bytes.len() as f64;
+    for frequency in scores.values_mut() {
+        *frequency = *frequency * 100.0 / len;
     }
 
-    return true;
-}
-
-fn is_ascii_char(byte: &u8) -> bool {
-    return (byte >= &ASCII_UPPER_A && byte <= &ASCII_UPPER_Z)
-        || (byte >= &ASCII_LOWER_A && byte <= &ASCII_LOWER_Z);
-}
-
-fn is_ascii_sign(byte: &u8) -> bool {
-    return byte < &ASCII_NUM_0
-        || (byte > &ASCII_NUM_9 && byte < &ASCII_UPPER_A)
-        || (byte > &ASCII_UPPER_Z && byte < &ASCII_LOWER_A)
-        || byte > &ASCII_LOWER_Z;
-}
-
-fn get_ascii_char_sign_ratio(bytes: &[u8]) -> f32 {
-    let bytes_len = bytes.len();
-    let signs = bytes.iter().filter(|x| is_ascii_sign(x)).count();
-    let chars = bytes_len - signs;
-    return signs as f32 / chars as f32;
-}
-
-fn get_ascii_char_space_ratio(bytes: &[u8]) -> f32 {
-    let bytes_len = bytes.len();
-    let spaces = bytes.iter().filter(|x| x == &&ASCII_SPACE).count();
-    let chars = bytes_len - spaces;
-    return spaces as f32 / chars as f32;
+    return scores;
 }
 
 #[cfg(test)]
