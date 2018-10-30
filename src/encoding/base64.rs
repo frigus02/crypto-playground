ec966bae4c9681eb558dc68586edfc05b69b04ef
Jan Kuehle <jkuehle90@gmail.com>
2018-10-30 21:31:45 +0000

    Add implementation for base64 decoding

--- src/encoding/base64.rs
+++ src/encoding/base64.rs
@@ -7,7 +7,7 @@
 pub fn encode(bytes: &[u8]) -> String {
     let bytes_len = bytes.len();
 
-    let base64_bytes_len = ((bytes_len as f64) * 8.0 / 6.0).ceil() as usize;
+    let base64_bytes_len = (bytes_len as f64 * 8.0 / 6.0).ceil() as usize;
     let mut base64_bytes = Vec::with_capacity(base64_bytes_len);
 
     for i in 0..base64_bytes_len {
@@ -46,7 +46,51 @@ fn encode_value(value: u8) -> u8 {
 }
 
 pub fn decode(base64: &str) -> Vec<u8> {
-    return vec![];
+    let base64_bytes = base64.as_bytes();
+    let base64_bytes_len = base64_bytes.len();
+
+    let bytes_len = (base64_bytes_len as f64 * 6.0 / 8.0).ceil() as usize;
+    let mut bytes = Vec::with_capacity(bytes_len);
+
+    for i in 0..bytes_len {
+        let first_index = (i as f64 / 6.0 * 8.0).floor() as usize;
+        let mut part1 = decode_value(base64_bytes[first_index]);
+        let mut part2 = if first_index + 1 < base64_bytes_len {
+            decode_value(base64_bytes[first_index + 1])
+        } else {
+            0
+        };
+
+        if i % 3 == 0 {
+            part1 = part1 << 2;
+            part2 = part2 >> 4;
+        } else if i % 3 == 1 {
+            part1 = part1 << 4;
+            part2 = part2 >> 2;
+        } else {
+            part1 = part1 << 6;
+        }
+
+        bytes.push(part1 + part2);
+    }
+
+    return bytes;
+}
+
+fn decode_value(value: u8) -> u8 {
+    return if value >= 65 && value <= 90 {
+        value - 65 // "A" - "Z"
+    } else if value >= 97 && value <= 122 {
+        value - 71 // "a" - "z"
+    } else if value >= 48 && value <= 57 {
+        value + 4 // "0" - "9"
+    } else if value == 43 {
+        62 // "+"
+    } else if value == 47 {
+        63 // "/"
+    } else {
+        panic!("Invalid base64 value: {}", value);
+    };
 }
 
 #[cfg(test)]
